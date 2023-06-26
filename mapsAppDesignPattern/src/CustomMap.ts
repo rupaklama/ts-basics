78288caa4822baa7b959cfa24b2f87c775a9ca32
Rupak Lama <rupaklamadev@gmail.com>
2023-06-26 15:11:44 -0400

    added map markers

---
+++ mapsAppDesignPattern/src/CustomMap.ts
@@ -0,0 +1,39 @@
+/// <reference types="@types/google.maps" />
+
+import { User } from "./User";
+import { Company } from "./Company";
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 2,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  // using class variable to refer to Class Type as well
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  }
+}
--- mapsAppDesignPattern/src/index.ts
+++ mapsAppDesignPattern/src/index.ts
@@ -1,12 +1,12 @@
-/// <reference types="@types/google.maps" />
+import { User } from "./User";
+import { Company } from "./Company";
 
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const user = new User();
+const company = new Company();
+
+const map = new CustomMap("map");
+
+map.addUserMarker(user);
+map.addCompanyMarker(company);
